03a4703cbdcdbd084df3f911e66ca04aee4b1a36
Achal Pandey <pandeyachal.ap@gmail.com>
2020-01-05 15:04:14 +0530

    Procedural Sphere seems to be working fine!

--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -1,69 +1,142 @@
-#define _USE_MATH_DEFINES
-
 #include "Sphere.h"
+
+#include "Interaction.h"
 #include "Ray.h"
+#include "Transform.h"
 
 #include "glm/geometric.hpp"
 
 #include <algorithm>
-#include <cmath>
 
 namespace Hikari
 {
 
-inline bool solveQuadratic(float a, float b, float c, float& alpha, float& beta)
+inline bool Quadratic(float a, float b, float c, float* t0, float* t1)
 {
     float discriminant = b * b - 4.f * a * c;
     if (discriminant < 0.f)
         return false;
     else if (discriminant == 0.f)
-        alpha = beta = -0.5 * (b / a);
+        *t0 = *t1 = -0.5 * (b / a);
     else
     {
-        float q = b > 0.f ? -0.5f * (b + sqrt(discriminant)) : -0.5f * (b - sqrt(discriminant));
-        alpha = q / a;
-        beta = c / q;
+        float rootDiscriminant = sqrt(discriminant);
+        float q = b > 0.f ? -0.5f * (b + rootDiscriminant) : -0.5f * (b - rootDiscriminant);
+        *t0 = q / a;
+        *t1 = c / q;
+
+        if (*t0 > *t1) std::swap(*t0, *t1);
     }
     return true;
 }
 
-bool Sphere::Intersect(const Ray& ray, float* distance) const
+bool Sphere::Intersect(const Ray& r, float* t, Interaction* interaction) const
 {
+    float phi;
+    glm::vec3 pHit;
+
+    Ray ray = (*m_WorldToObject)(r);
+
     float a = glm::dot(ray.m_Direction, ray.m_Direction);
-    float b = 2.f * glm::dot(ray.m_Direction, ray.m_Origin - m_Center);
-    float c = glm::dot(ray.m_Origin - m_Center, ray.m_Origin - m_Center) - (m_Radius * m_Radius);
+    float b = 2.f * glm::dot(ray.m_Direction, ray.m_Origin);
+    float c = glm::dot(ray.m_Origin, ray.m_Origin) - (m_Radius * m_Radius);
+
     float t0, t1;
-
-    if (!solveQuadratic(a, b, c, t0, t1))
-        return false;
-
-    if (t0 < 0.f && t1 < 0.f)
-        return false;
-
-    if (distance)
+    if (!Quadratic(a, b, c, &t0, &t1))
     {
-        if (t0 > 0.f && t1 > 0.f)
-            *distance = std::min(t0, t1);
-        else if (t0 > 0.f)
-            *distance = t0;
-        else
-            *distance = t1;
+        return false;
     }
-    return true;
-}
 
-glm::vec3 Sphere::GetSurfaceNormal(const glm::vec3& hitPoint) const
-{
-    return glm::normalize(hitPoint - m_Center);
-}
+    // This works because, `Quadratic` gurantees that `t0` is always less than `t1`
+    if (t0 > ray.m_tMax || t1 <= 0.f)
+    {
+        return false;
+    }
 
-glm::vec2 Sphere::GetTextureCoordinates(const glm::vec3& hitPoint) const
-{
-    return glm::vec2
-    (
-        ((std::atan2(hitPoint.x, hitPoint.z) / M_PI) + 1.f) * 0.5f,
-        std::acosf(hitPoint.y / m_Radius) / M_PI
-    );
+    float tShapeHit = t0;
+    if (tShapeHit <= 0.f)
+    {
+        tShapeHit = t1;
+        if (tShapeHit > ray.m_tMax)
+        {
+            return false;
+        }
+    }
+
+    pHit = ray(tShapeHit);
+    phi = std::atan2(pHit.z, pHit.x);
+
+    // `std::atan2` returns values in the range [-Pi, +Pi] so convert it to
+    // the range [0, 2 * PI)
+    if (phi < 0.f) phi += 2.f * M_PI;
+
+    // Test against clipping params.
+    // To test against "y" clipping params it is important to first check
+    // if the values `m_YMin` or `m_YMax` actually clips the sphere - which
+    // is done with the `m_YMin > -m_Radius` line, otherwise some intersections
+    // which lie just above the surface of the sphere (due to floating-point error)
+    // may be misinterpreted.
+    if ((m_YMin > -m_Radius && pHit.y < m_YMin) || (m_YMax < m_Radius && pHit.y > m_YMax)
+        || phi > m_PhiMax)
+    {
+        if (tShapeHit == t1) return false;
+        if (t1 > ray.m_tMax) return false;
+
+        tShapeHit = t1;
+
+        // Compute hitpoint and phi again for the case when `tShapeHit` = `t1`
+        pHit = ray(tShapeHit);
+        phi = std::atan2(pHit.y, pHit.x);
+        if (phi < 0.f) phi += 2.f * M_PI;
+
+        if ((m_YMin > -m_Radius && pHit.y < m_YMin) ||
+            (m_YMax < m_Radius && pHit.y > m_YMax) ||
+            phi > m_PhiMax)
+            return false;
+    }
+
+    float u = phi / m_PhiMax;
+    float theta = std::acos(glm::clamp(pHit.y / m_Radius, -1.f, 1.f));
+    float v = (theta - m_ThetaMin) / (m_ThetaMax - m_ThetaMin);
+
+    // Compute `dpdu` and `dpdv`
+    float yRadius = std::sqrt(pHit.x * pHit.x + pHit.z * pHit.z);
+    float invYRadius = 1.f / yRadius;
+    float cosPhi = pHit.x * invYRadius;
+    float sinPhi = pHit.z * invYRadius;
+    glm::vec3 dpdu(-m_PhiMax * pHit.z, 0.f, m_PhiMax * pHit.x);
+    glm::vec3 dpdv =
+        (m_ThetaMax - m_ThetaMin) * glm::vec3(pHit.y * cosPhi, -m_Radius * std::sin(theta), pHit.y * sinPhi);
+
+    // Compute auxiliary things for calculating coefficients for fundamental forms
+    glm::vec3 d2pduu = -m_PhiMax * m_PhiMax * glm::vec3(pHit.x, 0.f, pHit.z);
+    glm::vec3 d2pdudv = m_PhiMax * pHit.y * (m_ThetaMax - m_ThetaMin) * glm::vec3(-sinPhi, 0.f, cosPhi);
+    glm::vec3 d2pdvv = -(m_ThetaMax - m_ThetaMin) * (m_ThetaMax - m_ThetaMin) * glm::vec3(pHit.x, pHit.y, pHit.z);
+
+    // Compute coefficients for fundamental forms
+    float E = glm::dot(dpdu, dpdu);
+    float F = glm::dot(dpdu, dpdv);
+    float G = glm::dot(dpdv, dpdv);
+    glm::vec3 normal = glm::normalize(glm::cross(dpdv, dpdu));
+    float e = glm::dot(normal, d2pduu);
+    float f = glm::dot(normal, d2pdudv);
+    float g = glm::dot(normal, d2pdvv);
+
+    // Compute `dndu` and `dndv`
+    float invEGF2 = 1.f / (E * G - F * F);
+    glm::vec3 dndu = glm::vec3((f * F - e * G) * invEGF2 * dpdu +
+        (e * F - f * E) * invEGF2 * dpdv);
+    glm::vec3 dndv = glm::vec3((g * F - f * G) * invEGF2 * dpdu +
+        (f * F - g * E) * invEGF2 * dpdv);
+
+    // *interaction = (*m_ObjectToWorld)(Interaction(pHit, glm::vec2(u, v), dpdu, dpdv, dndu, dndv, this));
+    Interaction objectSpaceInteraction;
+    objectSpaceInteraction.m_HitPoint = pHit;
+    objectSpaceInteraction.m_Normal = normal;
+    objectSpaceInteraction.m_Albedo = m_Albedo;
+    *interaction = (*m_ObjectToWorld)(objectSpaceInteraction);
+
+    return true;   
 }
 
 }   // namespace Hikari
--- src/Sphere.h
+++ src/Sphere.h
@@ -1,30 +1,54 @@
 #pragma once
 
+#define _USE_MATH_DEFINES
+
 #include "glm/vec2.hpp"
 #include "glm/vec3.hpp"
+#include "glm/common.hpp"
+#include "glm/trigonometric.hpp"
+
+#include <algorithm>
+#include <math.h>
 
 namespace Hikari
 {
 
+class Interaction;
 class Ray;
+class Transform;
 
 class Sphere
 {
 public:
-    float m_Radius;
-    glm::vec3 m_Center;
-    glm::vec3 m_Albedo;
-
     Sphere(
+        const Transform*    objectToWorld,
+        const Transform*    worldToObject,
+        bool                reverseOrientation,
         float               radius,
-        const glm::vec3& 	center,
+        float               yMin,
+        float               yMax,
+        float               phiMax = 2.f * M_PI,
         const glm::vec3& 	albedo = glm::vec3(0.18f))
-        : m_Radius(radius), m_Center(center), m_Albedo(albedo)
+        : m_ObjectToWorld(objectToWorld), m_WorldToObject(worldToObject),
+        m_ReverseOrientation(reverseOrientation), m_Radius(radius),
+        m_YMin(glm::clamp(std::min(yMin, yMax), -radius, radius)),
+        m_YMax(glm::clamp(std::max(yMin, yMax), -radius, radius)),
+        m_ThetaMin(std::acos(glm::clamp(m_YMin / radius, -1.f, 1.f))),
+        m_ThetaMax(std::acos(glm::clamp(m_YMax / radius, -1.f, 1.f))),
+        m_PhiMax(glm::radians(glm::clamp(phiMax, 0.f, 360.f))),
+        m_Albedo(albedo)
     {}
 
-    bool Intersect(const Ray& ray, float* distance) const;
-    glm::vec3 GetSurfaceNormal(const glm::vec3& hitPoint) const;
-    glm::vec2 GetTextureCoordinates(const glm::vec3& hitPoint) const;
+    bool Intersect(const Ray& ray, float* t, Interaction* interaction) const;
+
+private:
+    const Transform* m_ObjectToWorld;
+    const Transform* m_WorldToObject;
+    bool m_ReverseOrientation;
+    const float m_Radius;
+    const float m_YMin, m_YMax;
+    const float m_ThetaMin, m_ThetaMax, m_PhiMax;
+    glm::vec3 m_Albedo;
 };
 
 }   // namespace Hikari
